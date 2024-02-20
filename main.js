017b75a64d7b00576a3ef36be6d57ba783b95d13
Ayush Saini <140002705+Ayushsainii04@users.noreply.github.com>
2024-02-20 15:38:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var tablinks1 = document.getElementsByClassName("tab-links1");
+        var tabcontents1 = document.getElementsByClassName("tab-contents1");
+        function opentab(tabname){
+            for(tablinks of tablinks1){
+                tablinks.classlist.remove("active-link1");
+            }
+            for(tabcontents of tabcontents1){
+                tabcontents.classlist.remove("active-tab1");
+            }
+            event.currentTarget.classlist.add("active-link1");
+            document.getElementById(tabname).classList.add("active-tab1");
+        }
